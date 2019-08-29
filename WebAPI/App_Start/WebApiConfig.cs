574f38aaf86554a08dd838a60dfe36e5827c1f61
faruk.ahsen@outlook.com <faruk.ahsen@outlook.com>
2019-08-29 23:09:05 +1000

    first Commit after staging

---
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //()()Enable CORS
+           // config.EnableCors(new EnableCorsAttribute("http://localhost:4200", headers:"*",methods:"*"));
+            //config.EnableCors(new EnableCorsAttribute("http://ang7-dev1.ap-southeast-2.elasticbeanstalk.com", headers: "*", methods: "*"));
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
